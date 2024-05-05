b1256b957f01db76b30c1c99d73d9b5a23ba1e0c
Dan Nicholls <dannicholls12@gmail.com>
2024-05-05 16:55:40 +1000

    refactor(client): encapsulate socket logic in MyClient class

--- client.py
+++ client.py
@@ -3,13 +3,23 @@ import socket
 HOST = socket.gethostname()
 PORT = 9000
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
 
-    s.sendall(b"This is a client request")
+class MyClient:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
 
-    data = s.recv(1024)
-    print("Response:", repr(data))
-finally:
-    s.close()
+    def send_request(self):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((HOST, PORT))
+                s.sendall(b"This is a client request")
+                data = s.recv(1024)
+                print("Response:", repr(data))
+        finally:
+            s.close()
+
+
+if __name__ == "__main__":
+    client = MyClient(HOST, PORT)
+    client.send_request()
