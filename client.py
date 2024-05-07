c1d14cfcdbabbd07702d493cfe056408194e39ee
Dan Nicholls <dannicholls12@gmail.com>
2024-05-07 20:45:40 +1000

    feat(client): add argparse for dynamic host, port, and resource

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import time
+import argparse
 
 HOST = socket.gethostname()
 PORT = 9000
@@ -11,24 +12,60 @@ class MyClient:
         self.host = host
         self.port = port
 
-    def send_request(self):
+    def send_request(self, resource: str = "/"):
         # socket is implicitly closed by socket.socket.__exit__
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
                 s.connect((HOST, PORT))
                 # time.sleep(5)
-                s.sendall(b"GET /")
+                request = "GET " + resource
+                s.sendall(request.encode("utf-8"))
                 data = s.recv(1024)
                 print("Response:", repr(data))
             except socket.error as e:
                 print(f"Connection error: {e}")
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="client",
+        description="A simple HTTP client",
+        usage="%(prog)s [options] [resource]",
+    )
+
+    parser.add_argument(
+        "resource",
+        nargs="?",
+        type=str,
+        default="/",
+        help="The resource to fetch (default: /)",
+    )
+    parser.add_argument(
+        "-H",
+        "--host",
+        type=str,
+        default=socket.gethostname(),
+        help="Host to connect to (default: current hostname)",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=9000,
+        choices=range(1, 65536),
+        metavar="[1-65535]",
+        help="The port to connect on (default: 9000)",
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = parse_args()
 
     def create_client_and_send_request():
-        client = MyClient(HOST, PORT)
-        client.send_request()
+        client = MyClient(args.host, PORT)
+        client.send_request(args.resource)
 
     threads = []
 
